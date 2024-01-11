fe4d5ef2a25677ad3a3572325d10218d6f5bf07f
Akshat0904 <askansara17@gmail.com>
2024-01-11 15:10:49 +0530

    Component files seperated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
@@ -40,9 +11,9 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
+              image={CORE_CONCEPTS[0].image}
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
-              img={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
